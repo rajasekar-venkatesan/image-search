fd748f70fcac75903020616c0176cef0df24266b
Rajasekar Venkatesan <raja0046@e.ntu.edu.sg>
2021-03-27 11:41:49 +0530

    Initial commit

---
+++ host_streamlit.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from PIL import Image
+from image_search import *
+
+"""
+# Image Search from Free Text
+
+Given a free text query, search through the images directory to find the most relevant images
+"""
+
+query = st.text_input("Enter Search Query: ", "trees near beach")
+num_results = st.slider("Num Search Results", 1, 5, 3, 1)
+
+results = search(query, num_results)
+for item in results:
+    st.write(item[0])
+    st.write(Image.open(item[1]))
---
+++ image_search.py
@@ -0,0 +1,67 @@
+import sentence_transformers
+from sentence_transformers import SentenceTransformer, util
+from PIL import Image
+import glob
+import torch
+import pickle
+import zipfile
+# from IPython.display import display
+# from IPython.display import Image as IPImage
+import os
+# from tqdm.autonotebook import tqdm
+from tqdm import tqdm
+torch.set_num_threads(4)
+
+
+# First, we load the respective CLIP model
+model = SentenceTransformer('clip-ViT-B-32')
+
+# Next, we get about 25k images from Unsplash
+img_folder = 'photos/'
+if not os.path.exists(img_folder) or len(os.listdir(img_folder)) == 0:
+    os.makedirs(img_folder, exist_ok=True)
+
+    photo_filename = 'unsplash-25k-photos.zip'
+    if not os.path.exists(photo_filename):  # Download dataset if does not exist
+        util.http_get('http://sbert.net/datasets/' + photo_filename, photo_filename)
+
+    # Extract all images
+    with zipfile.ZipFile(photo_filename, 'r') as zf:
+        for member in tqdm(zf.infolist(), desc='Extracting'):
+            zf.extract(member, img_folder)
+
+use_precomputed_embeddings = True
+
+if use_precomputed_embeddings:
+    emb_filename = 'unsplash-25k-photos-embeddings.pkl'
+    if not os.path.exists(emb_filename):  # Download dataset if does not exist
+        util.http_get('http://sbert.net/datasets/' + emb_filename, emb_filename)
+
+    with open(emb_filename, 'rb') as fIn:
+        img_names, img_emb = pickle.load(fIn)
+    print("Images:", len(img_names))
+else:
+    img_names = list(glob.glob('unsplash/photos/*.jpg'))
+    print("Images:", len(img_names))
+    img_emb = model.encode([Image.open(filepath) for filepath in img_names], batch_size=128, convert_to_tensor=True,
+                           show_progress_bar=True)
+
+
+# Next, we define a search function.
+def search(query, k=3):
+    # First, we encode the query (which can either be an image or a text string)
+    query_emb = model.encode([query], convert_to_tensor=True, show_progress_bar=False)
+
+    # Then, we use the util.semantic_search function, which computes the cosine-similarity
+    # between the query embedding and all image embeddings.
+    # It then returns the top_k highest ranked images, which we output
+    hits = util.semantic_search(query_emb, img_emb, top_k=k)[0]
+
+    # print("Query:")
+    # display(query)
+    results = []
+    for hit in hits:
+        results.append((img_names[hit['corpus_id']], os.path.join(img_folder, img_names[hit['corpus_id']])))
+        # print(img_names[hit['corpus_id']])
+        # display(IPImage(os.path.join(img_folder, img_names[hit['corpus_id']]), width=200))
+    return results
